b803cc0b565747990871279b3a17718987fa56d2
Shreya Vishwakarma <shreya@appsimpact.com>
2021-06-19 23:00:11 +0530

    Finished part 4

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Creating users'
+
+100.times do |i|
+  User.create(
+    name: Faker::Name.name_with_middle,
+    email: Faker::Internet.email,
+    address: Faker::Address.full_address,
+    contact: Faker::PhoneNumber.cell_phone_with_country_code
+  )
+end
+
+puts '100 users have been created'
